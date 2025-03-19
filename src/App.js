363cdbbf6ae6db430b0dd67514bed607ce4a0011
Gnanashekar K <gnanashekar045@gmail.com>
2025-03-19 22:20:14 +0530

    login page complition

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Conponents/Footer/Footer';
+import men_banner from './Conponents/Assets/banner_mens.png';
+import women_banner from './Conponents/Assets/banner_women.png';
+import kids_banner from './Conponents/Assets/banner_kids.png';
 
 
 function App() {
@@ -16,9 +19,9 @@ function App() {
         <NaveBar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory category="men" />} />
-          <Route path='/womens' element={<ShopCategory category="women" />} />
-          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=':productId' element={<Product />} />
           </Route>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import '../Pages/LoginSignup.css'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Enter your email' />
+          <input type="password" placeholder='Enter your password' />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already you have and account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing. i agree to the terms and conditions and use of the privacy policy </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
