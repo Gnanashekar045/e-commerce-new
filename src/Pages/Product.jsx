00cce6b1ef883f93ecc5a24adcc37bcea8d8202e
Gnanashekar K <gnanashekar045@gmail.com>
2025-03-20 06:04:18 +0530

    product commponent commited

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContect';  // Corrected import
+import { useParams } from 'react-router-dom';
+import BreadCrums from '../Conponents/BreadCrums/BreadCrums';
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
+  const { all_products } = useContext(ShopContext);  // Correct context usage
+  const { id } = useParams();
+  const product = all_products.find((e) => e.id === Number(id)); // Ensure type match
 
-export default Product
+  return (
+    <div>
+      <BreadCrums product={product} />
+    </div>
+  );
+};
+
+export default Product;
